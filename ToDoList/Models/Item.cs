16ca0661d72a93442a3bf70dba420205c91e94e3
Dana <dcluukko@gmail.com>
2020-03-16 16:11:27 -0700

    refactor database testing. continue debug.
    Co-authored-by: Dana Luukko <dcluukko@gmail.com>
    Co-authored-by: Steph Podolak <stephpodolak@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -3,60 +3,58 @@ using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
+  
   public class Item
   {
-    public int ItemId { get; set; }
     public string Description { get; set; }
-    public int CategoryId { get; set; }
-    public virtual Category Category { get; set; }
-  
-  public Item(string description, int itemId)
+    public int Id { get; }
+
+    public Item(string description)
     {
-        Description = description;
-        ItemId = itemId;
+      Description = description;
     }
 
-   public static void ClearAll()
-  {
-    MySqlConnection conn = DB.Connection();
-     conn.Open();
-     var cmd = conn.CreateCommand() as MySqlCommand;
-     cmd.CommandText = @"DELETE FROM items;";
-     cmd.ExecuteNonQuery();
-     conn.Close();
-     if (conn != null)
-     {
-      conn.Dispose();
-     }
-  }
-
-     public static Item Find(int searchId)
-  {
-    Item placeholderItem = new Item("placeholder item");
-    return placeholderItem;
-  }
-
-  public static List<Item> GetAll()
-  {
-    List<Item> allItems = new List<Item> { };
-    MySqlConnection conn = DB.Connection();
-    conn.Open();
-    MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-    cmd.CommandText = @"SELECT * FROM items;";
-    MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-    while (rdr.Read())
+    public static void ClearAll()
     {
-        int itemId = rdr.GetInt32(0);
-        string itemDescription = rdr.GetString(1);
-        Item newItem = new Item(itemDescription, itemId);
-        allItems.Add(newItem);
-    }
-    conn.Close();
-    if (conn != null)
-    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM items;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
         conn.Dispose();
+      }
     }
-    return allItems;
-  }
+
+    public static Item Find(int searchId)
+    {
+      Item placeholderItem = new Item("placeholder item");
+      return placeholderItem;
+    }
+
+    public static List<Item> GetAll()
+    {
+      List<Item> allItems = new List<Item> { };
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+          int itemId = rdr.GetInt32(0);
+          string itemDescription = rdr.GetString(1);
+          Item newItem = new Item(itemDescription);
+          allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+          conn.Dispose();
+      }
+      return allItems;
+    }
   }
 }
